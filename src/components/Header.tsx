390027de7a02e8dcf40166cdf50be6ff5415b054
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-15 21:53:46 +0900

    Style: Header 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
-import style from "../styles/Header.module.scss"
+import style from "../styles/Header.module.scss";
 const Header = () => {
   return (
     <header className={style.header}>
       <nav className={style.nav}>
         <div className={style.logo}>
-          <a href="/"><img src="/images/logo.png" alt="Logo" /></a>
+          <a href="/">
+            <img src="/images/logo.png" alt="Logo" />
+          </a>
         </div>
-        <div className={style.loginBtn}>
-          <a href="/login"><button>로그인</button></a>
+        <div className={style["login-btn"]}>
+          <a href="/login">로그인</a>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
