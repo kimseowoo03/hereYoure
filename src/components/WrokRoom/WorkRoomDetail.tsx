1fcfc08244eabf27c8a81cf672f40f77d10c19a7
kimseowoo03 <kimseowoo03@gmail.com>
2023-05-14 23:20:14 +0900

    Feat: 근무자 등록 모달 추가 및 UI반응형 작업 완료(#8)

--- src/components/WrokRoom/WorkRoomDetail.tsx
+++ src/components/WrokRoom/WorkRoomDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import { AxiosError } from "axios";
@@ -6,6 +6,8 @@ import style from "../../styles/WorkRoom/WorkRoom.module.scss";
 import { useAccessToken } from "../../store/useAccessTokenState";
 import api from "../../axiosConfig";
 import WorkerCard from "./WorkerCard";
+import useUIState from "../../store/useUIState";
+import WorkerRegisterModal from "../../modals/WorkerRegisterModal";
 
 export interface IWORKER_DATA {
   id: number;
@@ -52,6 +54,8 @@ const WORKERDATA:IWORKER_DATA[] = [
 
 const WorkRoomDetail = () => {
   const { workroom } = useParams();
+
+  const {workerRegisterModalOpen, setWorkerRegisterModalOpen} = useUIState()
   
   const {accessToken, setAccessToken} = useAccessToken()
   useEffect(()=> {
@@ -86,59 +90,62 @@ const WorkRoomDetail = () => {
     WorkRoomDetailData();
   })
   return (
-    <div className={style.layout}>
-      <div className={style.content}>
-        <div className={style.breadcrum}>
-          <p>근무방 목록 {">"} 인터라켓PC</p>
-        </div>
-        <div className={style["workroom-info-contnent"]}>
-          <div className={style["include-box"]}>
-            <div>주휴수당</div>
-            {/* {props.nightInclude && <div>주휴수당</div>}
+    <Fragment>
+      {workerRegisterModalOpen && <WorkerRegisterModal />}
+      <div className={style.layout}>
+        <div className={style.content}>
+          <div className={style.breadcrum}>
+            <p>근무방 목록 {">"} 인터라켓PC</p>
+          </div>
+          <div className={style["workroom-info-contnent"]}>
+            <div className={style["include-box"]}>
+              <div>주휴수당</div>
+              {/* {props.nightInclude && <div>주휴수당</div>}
           {props.overtimeInclude && <div>연휴수당</div>}
           {props.weeklyInclude && <div>야근수당</div>} */}
-          </div>
-          <div className={style["workroom-info"]}>
-            <h1>인터라켓PC</h1>
-            <button className={style["user-edit-button"]}>정보 수정</button>
-          </div>
-        </div>
-        <div className={style["worker-list"]}>
-          <div className={style.info}>
-            <div>
-              <h2>근무자 목록</h2>
-              <p>근무자 선택후, 근무자의 정보를 자세히 보실 수 있습니다.</p>
             </div>
-            <button>근무자 등록</button>
+            <div className={style["workroom-info"]}>
+              <h1>인터라켓PC</h1>
+              <button className={style["user-edit-button"]}>정보 수정</button>
+            </div>
           </div>
-          <div className={style["list-info"]}>
-            <p>이름</p>
-            <p>나이</p>
-            <p>성별</p>
-            <p>전화번호</p>
+          <div className={style["worker-list"]}>
+            <div className={style.info}>
+              <div>
+                <h2>근무자 목록</h2>
+                <p>근무자 선택후, 근무자의 정보를 자세히 보실 수 있습니다.</p>
+              </div>
+              <button onClick={setWorkerRegisterModalOpen}>근무자 등록</button>
+            </div>
+            <div className={style["list-info"]}>
+              <p>이름</p>
+              <p>나이</p>
+              <p>성별</p>
+              <p>전화번호</p>
+            </div>
+            <div className={style.list}>
+              <ul>
+                {WORKERDATA.map((worker) => {
+                  return (
+                    <WorkerCard
+                      key={worker.id}
+                      name={worker.name}
+                      id={worker.id}
+                      age={worker.age}
+                      gender={worker.gender}
+                      phoneNumber={worker.phoneNumber}
+                    />
+                  );
+                })}
+              </ul>
+            </div>
           </div>
-          <div className={style.list}>
-            <ul>
-              {WORKERDATA.map((worker) => {
-                return (
-                  <WorkerCard
-                    key={worker.id}
-                    name={worker.name}
-                    id={worker.id}
-                    age={worker.age}
-                    gender={worker.gender}
-                    phoneNumber={worker.phoneNumber}
-                  />
-                );
-              })}
-            </ul>
+          <div className={style["workroom-del-btn"]}>
+            <button>근무방 삭제</button>
           </div>
-        </div>
-        <div className={style["workroom-del-btn"]}>
-          <button>근무방 삭제</button>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
