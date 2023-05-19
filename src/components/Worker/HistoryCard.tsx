4cc8d17ca2f87cffc18362b6fde733b8c9edf143
kimseowoo03 <kimseowoo03@gmail.com>
2023-05-19 11:19:36 +0900

    Feat: 근무 카드 컴포넌트 분리 및 useMemo로 최적화 작업(#9)

--- src/components/Worker/HistoryCard.tsx
+++ src/components/Worker/HistoryCard.tsx
@@ -1,19 +1,29 @@
+import { useMemo } from "react";
 import style from "../../styles/Worker/HistoryCard.module.scss"
 import { IWORKER_HISTORY } from "./WorkerDetail";
 
 const HistoryCard = ({date, startTime, endTime, wage, cover}:IWORKER_HISTORY) => {
+  const formattedDate = useMemo(() => {
+    const week = ['일', '월', '화', '수', '목', '금', '토'];
+    const dayOfWeek = week[new Date(date).getDay()];
+    const parts = date.split("-");
+    const month = parts[1];
+    const day = parts[2];
+    return `${month}월 ${day}일 (${dayOfWeek})`;
+  }, [date]);
+
   return (
-    <li>
+    <li className={style.li}>
       <div>
         <input type="checkbox" />
       </div>
-      <p>{date}</p>
+      <p>{formattedDate}</p>
       <p>
-        {startTime} - {endTime}
+        {startTime} - {endTime} (4시간 30분)
       </p>
       <p>{wage}</p>
       <p>{cover}</p>
-      <button>수정</button>
+      <button className={style["work-edit-button"]}>수정</button>
     </li>
   );
 };
