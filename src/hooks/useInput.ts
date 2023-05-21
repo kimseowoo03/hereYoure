616b451af28b317927bad9a4b2404e8f261f0e22
kimseowoo03 <kimseowoo03@gmail.com>
2023-05-21 22:37:30 +0900

    Add: useInput 여러 에러문구를 위한 errorTesxt 상태 추가

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -8,9 +8,11 @@ export interface UseInputReturn {
   inputTouched: boolean;
   value: string | number;
   inputVaild: boolean;
+  errorText: string;
   onChange: (event: InputChangeEvent) => void;
   onBlurTouch: (touched: boolean) => void;
   checkVaild: (checked: boolean) => void;
+  setErrorMessage: (errorMessage: string) => void;
   reset: () => void;
 }
 
@@ -18,10 +20,15 @@ const useInput = (initialValue: initialValue): UseInputReturn => {
   const [value, setValue] = useState(initialValue);
   const [inputTouched, setInputTouched] = useState(false);
   const [inputVaild, setInputVaild] = useState(false);
+  const [errorText, setErrorText] = useState("");
 
   const onChange = useCallback((event: InputChangeEvent) => {
     setValue(event.target.value);
   }, []);
+
+  const setErrorMessage = (errorMessage: string) => {
+    setErrorText(errorMessage)
+  };
 
   const onBlurTouch = (touched: boolean) => {
     setInputTouched(touched);
@@ -33,7 +40,7 @@ const useInput = (initialValue: initialValue): UseInputReturn => {
 
   const reset = useCallback(() => setValue(initialValue), [initialValue]);
 
-  return {inputTouched, inputVaild, value, checkVaild, onChange, onBlurTouch, reset };
+  return {inputTouched, errorText, inputVaild, value, checkVaild, onChange, onBlurTouch, setErrorMessage, reset };
 };
 
 export default useInput;
