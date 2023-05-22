a0fcccb0bd3ad905de78e46271043fdb4a0a2688
kimseowoo03 <kimseowoo03@gmail.com>
2023-05-22 14:34:44 +0900

    Feat: 근무방 삭제 모달 구현(#8)

---
+++ src/modals/WorkroomDeleteModal.tsx
@@ -0,0 +1,44 @@
+import api from "../axiosConfig";
+import { AxiosError } from "axios";
+import useUIState from "../store/useUIState";
+import DeleteModal from "./DeleteModal";
+import { useAccessToken } from "../store/useAccessTokenState";
+
+interface IWorkroomDeleteProps {
+  name: string;
+  id?: string;
+}
+
+const WorkroomDeleteModal = ({ name, id }: IWorkroomDeleteProps) => {
+  const { setWorkroomDeleteModalOpen } = useUIState();
+  const { accessToken } = useAccessToken();
+
+  const handleDeleteSubmit = async () => {
+    try {
+      const config = {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      };
+
+      const res = await api.delete(`/workroom?id=${id}`, config);
+      console.log(res);
+    } catch (error) {
+      const err = error as AxiosError;
+      if (!err.response) {
+        console.log("response가 없습니다.");
+      } else {
+        console.log(err);
+        console.warn(`error: ${err.message}`);
+      }
+    }
+  };
+
+  const workroomDeleteModalProps = {
+    title: "근무방",
+    deleteName: name,
+    cancleState: setWorkroomDeleteModalOpen,
+    handleSubmit: handleDeleteSubmit,
+  };
+  return <DeleteModal {...workroomDeleteModalProps} />;
+};
+
+export default WorkroomDeleteModal;
