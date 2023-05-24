3ebdf1ec8bfa241a076531202dfabe563fa3c022
kimseowoo03 <kimseowoo03@gmail.com>
2023-05-24 23:56:28 +0900

    Feat: 근무자 상세페이지 UI구현 및 조회 API연동(#9)

--- src/components/Worker/WorkerDetail.tsx
+++ src/components/Worker/WorkerDetail.tsx
@@ -1,6 +1,13 @@
+import { Fragment, useEffect } from "react";
 import style from "../../styles/Worker/WorkerDetail.module.scss";
 import HistoryCard from "./HistoryCard";
 import WorkerInfoCards from "./WorkerInfoCards";
+import api from "../../axiosConfig";
+import { useAccessToken } from "../../store/useAccessTokenState";
+import { useParams } from "react-router-dom";
+import DateDropdown from "./DateDropdown";
+import useUIState from "../../store/useUIState";
+import WorkerInfoFixModal from "../../modals/WokerInfoFixModal";
 
 export interface IWORKER_HISTORY {
   date: string;
@@ -79,55 +86,88 @@ const WORKER_HISTORY_DATA: IWORKER_HISTORY[] = [
 ];
 
 const WorkerDetail = () => {
+  const { workerid } = useParams();
+  const { accessToken, setAccessToken } = useAccessToken();
+
+  const { workerInfoFixModalOpen, setWorkerInfoFixModalOpen } = useUIState();
+
+  useEffect(() => {
+    const WokerDetailData = async () => {
+      try {
+        //근무자 상세데이터 조회
+        if (!accessToken) {
+          const storedAccessToken = localStorage.getItem("accessToken");
+          if (storedAccessToken) {
+            setAccessToken(storedAccessToken);
+          }
+        }
+
+        const config = {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
+        const res = await api.get(`/worker?id=${workerid}&month=${"5"}`, config);
+        console.log(res);
+      } catch (error) {}
+    };
+    WokerDetailData();
+  }, []);
+
   return (
-    <div className={style.layout}>
-      <div className={style.content}>
-        <div className={style.breadcrum}>
-          <p>
-            근무방 목록 {">"} 인터라켓PC{">"}근무정보
-          </p>
-        </div>
-        <article>
-          <section>
-            <div className={style["worker-info"]}>
-              <h1>
-                <span>김서우</span>님 근무정보
-              </h1>
-              <button className={style["user-edit-button"]}>정보 수정</button>
-            </div>
-            <WorkerInfoCards />
-          </section>
-          <section>
-            <div className={style["work-info"]}>
-              <h2>근무정보</h2>
-              <div>날짜 드롭다운</div>
-            </div>
-            <div className={style["work-content"]}>
-              <div className={style["work-header"]}>
-                <div>
-                  <p>전체선택</p>
-                  <button className={style["work-delete-button"]}>삭제</button>
+    <Fragment>
+      {workerInfoFixModalOpen && <WorkerInfoFixModal />}
+      <div className={style.layout}>
+        <div className={style.content}>
+          <div className={style.breadcrum}>
+            <p>
+              근무방 목록 {">"} 인터라켓PC{">"}근무정보
+            </p>
+          </div>
+          <article>
+            <section>
+              <div className={style["worker-info"]}>
+                <h1>
+                  <span>김서우</span>님 근무정보
+                </h1>
+                <button className={style["user-edit-button"]} onClick={setWorkerInfoFixModalOpen}>정보 수정</button>
+              </div>
+              <WorkerInfoCards />
+            </section>
+            <section>
+              <div className={style["work-info"]}>
+                <h2>근무정보</h2>
+                <DateDropdown />
+              </div>
+              <div className={style["work-content"]}>
+                <div className={style["work-header"]}>
+                  <div>
+                    <p>전체선택</p>
+                    <button className={style["work-delete-button"]}>
+                      삭제
+                    </button>
+                  </div>
+                  <button className={style["work-register-button"]}>
+                    근무등록
+                  </button>
                 </div>
-                <button className={style["work-register-button"]}>
-                  근무등록
-                </button>
+                <div className={style["work-list-info"]}>
+                  <p>근무날짜</p>
+                  <p>근무시간</p>
+                  <p>시급</p>
+                  <p>대리출근</p>
+                </div>
+                <ul className={style["work-list-contnet"]}>
+                  {WORKER_HISTORY_DATA.map((history, index) => {
+                    return <HistoryCard key={index} {...history} />;
+                  })}
+                </ul>
               </div>
-              <div className={style["work-list-info"]}>
-                <p>근무날짜</p>
-                <p>근무시간</p>
-                <p>시급</p>
-                <p>대리출근</p>
-              </div>
-              <ul className={style["work-list-contnet"]}>
-                {WORKER_HISTORY_DATA.map((history, index) => {
-                  return <HistoryCard key={index} {...history} />;
-                })}
-              </ul>
-            </div>
-          </section>
-        </article>
+            </section>
+          </article>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 export default WorkerDetail;
